fc3395c1d2790a0ae69d425f7dd74ef975bb2498
Stephan Helas <stephan.helas@gmail.com>
2016-08-19 17:10:26 +0200

    Added Drop Procedure before create

--- ms-sql/sp_maintenancecleanup.sql
+++ ms-sql/sp_maintenancecleanup.sql
@@ -1,11 +1,23 @@
--- the definition of the procedure.
--- Author:		<Stephan,Helas>
+/*
+ * sp_maintenancecleanup.sql
+ * Copyright (C) 2016 Stephan Helas <stephan.helas@pp-nt.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
 
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE dbo.CommandLogCleanup
+
+IF EXISTS ( SELECT  *
+            FROM    sys.objects
+            WHERE   object_id = OBJECT_ID(N'CommandLogCleanup')
+            AND type IN ( N'P', N'PC' ) ) 
+DROP PROCEDURE [dbo].[CommandLogCleanup]
+GO
+
+CREATE PROCEDURE [dbo].[CommandLogCleanup]
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -13,3 +25,4 @@ BEGIN
 END
 GO
 
+-- vim:et sw=2 ts=2 expandtab:
